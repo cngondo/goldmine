73f2b57f4e19d793ae84c5048dd8904ca12d3125
Cornellius Ngondo <devngondo@gmail.com>
2015-08-10 15:10:34 +0000

    Final setup of the user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :token_authenticatable, :confirmable
+
+  before_save :ensure_authentication_token
+
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
+
+end
